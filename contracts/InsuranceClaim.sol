51f892652ac7b370cdbe3b7c97b12d917c56be34
Abraxasu <sturgeonkid@gmail.com>
2018-07-02 15:13:51 +0200

    Update
    Methods for making theft and damage claims added
    Method for validating claims added.
    changed claims to use int 1 - 4 to show state rather than a boolean

--- contracts/InsuranceClaim.sol
+++ contracts/InsuranceClaim.sol
@@ -6,11 +6,10 @@ contract InsuranceClaim is HelperFunctions
 {
  //TO Edit add insurance company with which the claim is being submitted to
     mapping(uint256 => Claim) Claims;//@Dev Keeps track of all claims paidout
-    mapping (uint256 => Claim) ToBeApproved;//@Dev pending claims that need approval
     uint256 TotalClaims;
     uint256 PendingApprovalsCount;
     ClaimsValidatorLedger InternalValidator;//@Dev used for delagating the verification of claims
-    constructor ()
+    constructor ( )
     {
       require(validtorAddress != address(0));//ensure valid address  
       TotalClaims=0;
@@ -37,14 +36,61 @@ contract InsuranceClaim is HelperFunctions
         emit GeneralLogger(message);
          return message;
      }
-     ToBeApproved[PendingApprovalsCount++]=Claim(claimerId,amount,CompanyId,false);
+     Claims[PendingApprovalsCount++]=Claim(claimerId,amount,CompanyId,false);
      
      message="Succesfully paid out claim";
      emit GeneralLogger(message);
      return message;
     }
-    //@Dev function that returns all the values
+    //@Dev Change a policy to make a claim for theft of the item. fails if the state of the claim is something other than holding.
+    function TheftClaim(uint256 Reference) returns (string message)
+    {
+        Claim temp = Claims[Reference];
+        temp.state = 2;
+        if(temp.state == 1)
+        {
+        message = "Claim is now pending. \nOnce an affidavit written by the police has been received by internal resources, the claim will be paid out.";
+        }
+        else
+        {
+            message = "This claim has either been paid out, expired or is already pending.";
 
+        }
+        GeneralLogger.emit(message);
+        return message;
+       
+    }
+
+    //@Dev Change a policy to make a claim for damage of the item. fails if the state of the claim is something other than holding.
+    function DamageClaim(uint256 Reference) returns (string message) 
+    {
+        Claim temp = Claims[Reference];
+        if (temp.state == 1)
+        {
+            temp.state = 2;
+            message = "Claim is now pending. \nOnce the shop where the item was purchased has confirmed the damage, the claim will be paid out.";
+        }
+        else
+        {
+            message = "This claim has either been paid out, expired or is already pending.";
+            
+        }
+        GeneralLogger.emit(message);
+        return message;
+    }
+    //@Dev this function is a stub. It will simply change the state of a claim received to paid out if state = 2
+    function Validification(uint256 Reference) returns (string message) 
+    {
+        Claim temp = Claims[Reference];
+        if (temp.state == 2)
+        {
+            temp.state = 3;
+            message = "Proof has been received.     \nThe claim will now be paid out in full to the customer"
+        }
+        else {message = "This policy has either been paid out already or no claim has been made on it.\nNo changes made."}
+        GeneralLogger.emit(message);
+        return message;
+    }
 
     //TODO Add other functions
     //Like approving claims
